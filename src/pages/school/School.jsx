e068da60914c39d1fce875511f4dc46b49cc0a79
TarushGupta23 <tarush23.gupta@gmail.com>
2024-06-04 13:44:39 +0530

    add school page basic structure

--- src/pages/school/School.jsx
+++ src/pages/school/School.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './school.css'
+import Button from "../../components/Button/BaseButton";
 
 export default function School() {
     /* ==============================================
@@ -9,7 +10,7 @@ export default function School() {
         <section id="school-hero-section">
             <div className="school-logo"><img src="images/org-logo.png" alt="" /></div>
             <div className="school-content">
-                <h2 className="school-name">Ryan International School, Ludhiana</h2>
+                <h2 className="school-name">Ryan International School, Ludhiana <Button text="View Website" /></h2>
                 <div className="hero-container">
                     <li>
                         <h4>Chairman</h4>  <span>Cybernauts</span>
@@ -24,14 +25,70 @@ export default function School() {
             </div>
         </section>
 
-        <section id="notices"></section>
+        <section id="notices">
+            <h2 className="section-heading">Notice</h2>
+            <div className="tab-container">
+                <ul>
+                    <div className="tab tab-selected">All</div>
+                    <div className="tab">State Head</div>
+                    <div className="tab">Org</div>
+                    <div className="tab">Staff</div>
+                </ul>
+            </div>
+            <div className="notice-content tab-content">
+                <ul>
+                    <li>Hello</li>
+                    <li>Hello</li>
+                    <li>Hello</li>
+                    <li>Hellog</li>
+                </ul>
+            </div>
+        </section>
         
-        <section id="money"></section>
+        <section id="money">
+            <h2 className="section-heading">Expenditure</h2>
+            {/* need to show expences in bar graph format, teachers salary, staff salary, busses, infrastructure, goals... */}
+            <div className="tab-container">
+                <ul>
+                    <div className="tab tab-selected">Overall</div>
+                    <div className="tab">Teacher Salary</div>
+                    <div className="tab">Workers Salary</div>
+                    <div className="tab">Infrastructure</div>
+                    <div className="tab">Other Expenditures</div>
+                    <div className="tab">Goals</div>
+                </ul>
+            </div>
+            <div className="tab-content">
+                hlo
+            </div>
+        </section>
 
-        <section id="staff-students"></section>
+        <section id="income">
+            <h2 className="section-heading">Fee Structure</h2>
+            <div className="tab-container">
+                <ul>
+                    <div className="tab">Tution Fee</div>
+                    <div className="tab">Bus Fee</div>
+                    <div className="tab">Savings</div>
+                </ul>
+            </div>
+            <div className="tab-content">
+                hllo
+            </div>
+        </section>
 
-        <section id="services"></section>
-
-        <section id="school-gallery"></section>
+        <section id="staff-students">
+            <h2 className="section-heading">Staff and Student details</h2>
+            <div className="tab-container">
+                <ul>
+                    <div className="tab">Teachers</div>
+                    <div className="tab">Workers</div>
+                    <div className="tab">Students</div>
+                </ul>
+            </div>
+            <div className="tab-content">
+                helo
+            </div>
+        </section>
     </>)
 }
